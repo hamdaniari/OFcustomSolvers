f5ffb162e63e4e47d2ec20a051c4edc4fce04630
Ari Hamdani <39043038+hamdaniari@users.noreply.github.com>
2022-06-14 15:29:19 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'icoHeatFoam'
+copyright = '2022, Hamdani'
+author = 'Hamdani'
 
 release = '0.1'
 version = '0.1.0'
